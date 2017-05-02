6a8715da3797e6b57827d055be95e53d6ec41862
Christopher Nicholson <nicholsonc@gmail.com>
2017-05-02 19:36:05 -0400

    Benford input stream reading but extra lines

--- Chapter02/Homework/Benford/Benford/Source.cpp
+++ Chapter02/Homework/Benford/Benford/Source.cpp
@@ -1,12 +1,36 @@
 #include<iostream>
+#include<fstream>
+#include<string>
+
 using namespace std;
+
+char getFirstDigit(string number)
+{
+	return number.at(0);
+}
+
+int charToIndex(char ch)
+{
+	return ch - '0';
+}
 
 int main()
 {
 	int digits[] = { 0,0,0,0,0,0,0,0,0,0 };
+	string number;
+	fstream input;
+
+	input.open("C:\\Projects\\CSA401\\Chapter02\\Homework\\pops.csv");
+
+	while (input >> number)
+		cout << number << endl;
+
+	input.close();
 
 	for (int i = 0; i < 10; i++)
 	{
 		cout << digits[i] << endl;
 	}
+
+
 }
