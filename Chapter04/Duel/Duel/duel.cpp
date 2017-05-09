864b13ce7937daf14ec46689afa7fc968f4f0cca
Christopher Nicholson <nicholsonc@gmail.com>
2017-05-09 19:48:26 -0400

    shoot functions stubbed and main added - runs

---
+++ Chapter04/Duel/Duel/duel.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+void Aaron_shoots(bool aaron, bool& bob, bool& charlie);
+void Bob_shoots(bool& aaron, bool bob, bool& charlie);
+void Charlie_shoots(bool& aaron, bool& bob, bool charlie);
+
+int main() {
+
+	return 0;
+}
+
+void Aaron_shoots(bool aaron, bool & bob, bool & charlie)
+{
+}
+
+void Bob_shoots(bool & aaron, bool bob, bool & charlie)
+{
+}
+
+void Charlie_shoots(bool & aaron, bool & bob, bool charlie)
+{
+}
