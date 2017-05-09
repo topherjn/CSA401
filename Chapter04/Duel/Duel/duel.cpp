0467db831aae28167cd3f3da793ec6639f15a693
Christopher Nicholson <nicholsonc@gmail.com>
2017-05-09 19:53:53 -0400

    variables in main declared, and global num duels

--- Chapter04/Duel/Duel/duel.cpp
+++ Chapter04/Duel/Duel/duel.cpp
@@ -4,13 +4,27 @@
 
 using namespace std;
 
+int main();
+
 void Aaron_shoots(bool aaron, bool& bob, bool& charlie);
 void Bob_shoots(bool& aaron, bool bob, bool& charlie);
 void Charlie_shoots(bool& aaron, bool& bob, bool charlie);
 
-int main() {
+const int NUM_DUELS = 10000;
 
-	return 0;
+int main()
+{
+
+	bool aaronAlive, bobAlive, charlieAlive;
+	int aaronWins = 0;
+	int bobWins = 0;
+	int charlieWins = 0;
+	int i;
+
+	srand(time(NULL));
+
+	for (i = 0; i < NUM_DUELS; i++)
+		return 0;
 }
 
 void Aaron_shoots(bool aaron, bool & bob, bool & charlie)
